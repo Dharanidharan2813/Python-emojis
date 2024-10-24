ac608781e4c8f38bd758c71d4266b12416ee4bc3
Dharanidharan2813 <dharanidharans1328@gmail.com>
2024-10-24 14:47:07 +0530

    emojis(24/10/2024)

---
+++ emoji.py
@@ -0,0 +1,154 @@
+#grinning face 
+print("\U0001F600""----Grinning face")
+
+#grinning face with big eyes 
+print("\U0001F603""----Grinning face with big eyes" )
+
+#grinning face with smiling eyes 
+print("\U0001F604""----Grinning face with smiling eyes")
+
+#beaming face with smiling eyes 
+print("\U0001F601"'----Beaming face with smiling eyes')
+
+#grinning squinting face 
+print("\U0001F606""----Grinning squinting face ")
+
+#grinning face with sweat
+print("\U0001F605""----Grinning face with sweat ")
+
+#rolling on the floor laughing 
+print("\U0001F923""----Rolling on the floor laughing")
+
+#face with tears of joy 
+print("\U0001F602""----Face with tears of joy ")
+
+# slightly smiling face
+print("\U0001F642""----Slightly smiling face")
+
+# upside-down face 
+print("\U0001F643""----upside-down face ")
+
+# winking face
+print("\U0001F609""----Winking face")
+
+# smiling face with smiling eyes 
+print("\U0001F60A""----Smiling face with smiling eyes ")
+
+#smiling face with halo 
+print("\U0001F607""----Smiling face with halo")
+
+#smiling face with 3 hearts
+print("\U0001F970""----Smiling face with 3 hearts ")
+
+# smiling face with heart-eyes
+print("\U0001F60D""----Smiling face with heart-eyes")
+
+# star-struck 
+print("\U0001F929""----star-struck ")
+
+# face blowing a kiss 
+print("\U0001F618""----face blowing a kiss ")
+
+#kissing face 
+print("\U0001F617""----kissing face ")
+
+#smiling face 
+print("\U0000263A""----smiling face ")
+
+# kissing face with closed eyes 
+print("\U0001F61A""---- kissing face with closed eyes ")
+
+# kissing face with smiling eyes 
+print("\U0001F619""----kissing face with smiling eyes ")
+
+# face savoring food 
+print("\U0001F60B""----face savoring food")
+
+#face with tongue 
+print("\U0001F61B""----face with tongue ")
+
+#winking face with tongue 
+print("\U0001F61C""----winking face with tongue")
+
+# zany face 
+print("\U0001F92A""----zany face ")
+
+# squinting face with tongue 
+print("\U0001F61D""----squinting face with tongue ")
+
+# money-mouth face 
+print("\U0001F911""----money-mouth face ")
+
+#hugging face 
+print("\U0001F917""----hugging face ")
+
+#face with hand over mouth 
+print("\U0001F92D""----face with hand over mouth ")
+
+#shushing face 
+print("\U0001F92B""----shushing face")
+
+#thinking face 
+print("\U0001F914""----thinking face ")
+
+#zipper-mouth face 
+print("\U0001F910""----zipper-mouth face ")
+
+#face with raised eyebrow 
+print("\U0001F928""----face with raised eyebrow ")
+
+#neutral face 
+print("\U0001F610""----neutral face ")
+
+#expressionless face 
+print("\U0001F611""----expressionless face")
+
+#face without mouth 
+print("\U0001F636""----face without mouth")
+
+#smirking face
+print("\U0001F60F""----smirking face")
+
+#unamused face 
+print("\U0001F612""----unamused face ")
+
+#face with rolling eyes 
+print("\U0001F644""----face with rolling eyes ")
+
+#grimacing face 
+print("\U0001F62C" "----grimacing face  ")
+
+#lying face 
+print("\U0001F925""----lying face")
+
+#relieved face 
+print("\U0001F60C""----relieved face")
+
+#pensive face 
+print("\U0001F614""----pensive face")
+
+#sleepy face 
+print("\U0001F62A""----sleepy face ")
+
+#drooling face 
+print("\U0001F924""----drooling face")
+
+#sleeping face 
+print("\U0001F634""----Sleeping face ")
+
+#face with medical mask
+print("\U0001F637""----Face with medical mask")
+
+#face with thermometer 
+print("\U0001F912""----Face with thermometer ")
+
+#face with head-bandage 
+print("\U0001F915""----Face with head-bandage")
+
+#nauseated face 
+print("\U0001F922""----Nauseated face ")
+
+
+
+
+
